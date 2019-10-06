2366cc8b1803618d9993dc96785aadcaf0f334d5
galinabeleva <belevagb@gmail.com>
2019-10-06 22:25:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double sum = 0.0;
+            int counter = 0;
+
+            while (counter >= 15)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade >= 4.00)
+                {
+                    sum += grade;
+                }
+                counter++;
+            }
+
+            double average = sum / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {average}");
+        }
+    }
+}
