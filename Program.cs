9f3102bcf9b85e66d716d350163657c13405c0d2
galinabeleva <belevagb@gmail.com>
2019-10-06 22:20:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+
+            while (counter <= n)
+            {
+                Console.WriteLine(counter);
+                counter = counter * 2 + 1;
+            }
+        }
+    }
+}
